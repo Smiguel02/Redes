d65c3d2c5b30ad655c6f8c9c43b8956c0a3da487
Smiguel02 <71400611+Smiguel02@users.noreply.github.com>
2022-04-04 10:47:42 +0100

    llopen files
    Apesar de ainda termos de por isto numa unica funçao, esta a funcionar para a proxima aula que é o que queremos, LETS GOOOO

--- ProtocolAPI_Recetor.c
+++ ProtocolAPI_Recetor.c
@@ -6,93 +6,79 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <signal.h>
-
-//Biblioteca para abrir a API como transmissor
+#include <math.h>
 
 #define BAUDRATE B9600
 #define MODEMDEVICE "/dev/ttyS10"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-#define TIMEOUT 3
 
 #define ESCBYTE 0x7d
 #define MAX_SIZE 255
 #define TRANSMITTER 1
 #define RECEIVER 0
 
-//Supervision/Unnumbered frames
 //novos defines para maquina de estados
 #define Flag 0x7e
-#define A 0x03 //Transmitter
-#define SET 0x03
-#define UA 0X07
-#define BCC_T A^SET
+#define A 0x03 //Receiver
+#define UA 0x07  
 #define BCC_R A^UA
+#define SET 0x03
 
-
-  int state=0;
-
+//
 volatile int STOP=FALSE;
 
 struct applicationLayer {
 int fileDescriptor; /*Descritor correspondente à porta série*/
 int status; /*TRANSMITTER | RECEIVER*/
+
 };
 
 struct linkLayer {
 char port[20]; /*Dispositivo /dev/ttySx, x = 0, 1*/
 int baudRate; /*Velocidade de transmissão*/
-unsigned int sequenceNumber; /*Número de sequência da trama: 0, 1, ...*/
-unsigned int timeout;   /*Valor do temporizador: 1 s*/
+unsigned int sequenceNumber; /*Número de sequência da trama: 0, 1*/
+unsigned int timeout; /*Valor do temporizador: 1 s*/
 unsigned int numTransmissions; /*Número de tentativas em caso de falha*/
 char frame[MAX_SIZE]; /*Trama*/
 };
 
 
-
 //Inicia comunicaçao entre transmitter(util=0) e receiver(util=1)
 //returns data connection id, or negative value in case of failure/error
-
-void tictoc(){
-  printf("Timedout bro\n");
-	state=0;
-}
+int llopen(int porta, int util){
 
 
-int llopen(int porta){
 
-  int i, res,  fd, k=0;
+  int state=0, i, res,fd;
   char *door=malloc(sizeof(char));
-  char input[5],output[5];
   struct applicationLayer app;  
   struct linkLayer layer;
-  struct termios oldtio,newtio;
-
-
+  struct termios oldtio, newtio;
+  char buf[5], input[5];
 
   //define port
-  *door='0'+porta;
+  door[0]='0'+porta;
   strcpy(layer.port, "/dev/ttyS");
   if(porta<10){
   strcat(layer.port, door);
   }else if(porta==10){
     strcat(layer.port, "10");
   }else     strcat(layer.port, "11");
+
   
   layer.baudRate=BAUDRATE;
   layer.timeout=3;
-  layer.numTransmissions=3;
 
-  //Criar informação num buffer de 5 elementos
     input[0]=Flag;
     input[1]=A;
-    input[2]=SET;
-    input[3]=BCC_T;
+    input[2]=UA;
+    input[3]=BCC_R;
     input[4]=Flag;
 
-  //abrir porta
+
+ 
   //COnfirma se inserimos bem no kernel
     if ((strcmp("/dev/ttyS10", layer.port)!=0) && 
   	      (strcmp("/dev/ttyS11", layer.port)!=0)) {
@@ -102,8 +88,7 @@ int llopen(int porta){
 
 
   /*
-    Open ser  printf("Port: %s")
-ial port device for reading and writing and not as controlling tty
+    Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.*/
   
   {
@@ -124,8 +109,8 @@ ial port device for reading and writing and not as controlling tty
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 0;   /* blocking read until 1 chars received */
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
 
 
 
@@ -145,93 +130,95 @@ ial port device for reading and writing and not as controlling tty
 
     printf("New termios structure set\n");
 
-
   }
-    
-  //inicializar alarme
-  (void) signal(SIGALRM, tictoc);
 
+    printf("antes switch \n");
 
-
-  //Maquina de estados para verificação da mensagem
-  k=0;
-  while(k<=layer.numTransmissions && state!=6){
-  switch(state){
-
-    //escrever informaçao e verificar
-    case 0:  
-		alarm(0);
-		printf("Estamos em state=0 pela %d vez\n",k);
-    if(k>layer.numTransmissions){
-      printf("Nao recebeste dados nenhuns amigo, problems de envio\n");break;
-    }
-    k++;
-    res=write(fd,input,5);
-    printf("%d Bytes written\n", res);
-    state=1;
-    alarm(layer.timeout);    
-break;
-
-    //receber informaçao
-    case 1:  
-		printf("ENtramos na funçao read\n");
-		while(!read(fd,&output[0],1)){}
+  //control field definition
+  while(state!=6){
+   //Receiver
+      switch(state){
+        case 0:  
+	    	printf("ENtramos na funçao read\n");
+		while(!state){
+		res=read(fd,&buf[0],1);
+    printf("%d Bytes Read\n", res);
+    if(res==1)break;
+	}
 		printf("saimos do primeiro read\n");
-		if(output[0]==Flag){
-			printf("Lemos a promeira flag\n");
-			state=2;
-			}else state=0;
-    
-    
+		if(buf[0]==Flag){
+			printf("Lemos a primeira flag\n");
+			state=1;
+			}else state=0;    
 break;
+
+
   //verificação da mensagem recebida, ja paramos o timer de rececao, porque somos fixes e recebemos tudo
-  case 2:
-  while(!read(fd,&output[1],1)){}
-  if(output[1]==A){
+  case 1:
+  res=0;
+  printf("ENtramso em state=1\n");
+  while(state && (!res)){
+  		res=read(fd,&buf[1],1);}
+      printf("%d Bytes Read\n", res);
+  if(buf[1]==A){
 	 printf("Address very well received!\n");
-	 state=3;
-	  }else state=0;
+   state=2;
+	  }else {state=0;printf("N recebemos o Address\n");}
 	  
 break;	  
 	  
-	  case 3:
-while(!read(fd,&output[2],1)){}
-	if(output[2]==UA){
-    printf("UA IS HERE WITH US TOOOOO!\n");
-    state=4;
+	  case 2:
+    res=0;
+while((state==2) && (!res)){
+		res=read(fd,&buf[2],1);}
+	if(buf[2]==SET){
+    printf("SET IS HERE WITH US TOOOOO!\n");
+    state = 3;
 }else state=0;
     
     
 break;    
-    case 4: 
-    while(!read(fd,&output[3],1)){}
-      if(output[3]==BCC_R){
+    case 3: 
+    res=0;
+   while((state==3) && (!res)){
+    res=read(fd,&buf[3],1);}
+      if(buf[3]==(SET^A)){
     printf("What, os bits de verificaçao(BCC) tambem estao bem? Crazyy\n");
-    state=5;
+    state = 4;
   }else state=0;printf("Erros de transmissao, repeat please bro!\n");
     
 break;    
-    case 5:
-    while(!read(fd,&output[4],1)){}
-    if(output[4]==Flag){
-    printf("RECEBEMOS TUDO EM CONDIÇOES, CONGRATULATIONS\n");state=6;
-  }else {state=0;printf("Erros de transmissao, repeat please bro!\n");}
+    case 4:
+    res=0;
+    while((state==4) && (!res)){
+    res=read(fd,&buf[4],1);}
+    if(buf[4]==Flag){
+    printf("RECEBEMOS TUDO EM CONDIÇOES, CONGRATULATIONS\n");
+    state = 5;
+    break;
+  }else state=0;printf("Erros de transmissao, repeat please bro!\n");
     
     break;
+     case 5:
+        //FAZER RETRANS
+        res=write(fd, input, 5);
+        printf("%d Bytes written\n", res);
+        state=6;
+        break;   
     
-  
+  }
 
   }
-  }
-  printf("WE DID IT MOTHERFUCKERS\n");
+    printf("Finished everything \n");
 
+	  sleep(1);
+    return 0;
 }
 
-int main(){
-  llopen(10);
-}
-
-
+int main(int argc, char** argv)
+{
   
- 
+  llopen(11,0);
+    return 0;
+}
 
